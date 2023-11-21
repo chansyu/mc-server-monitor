244b7a2f4f7b1837b1d19fe8d64b01b29405623e
Steven Chan <stevenychansc@gmail.com>
2023-11-20 16:35:16 -0800

    reveal errors in frontend

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -18,13 +18,18 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 func (app *application) seed(w http.ResponseWriter, r *http.Request) {
 	s, err := app.remoteConsole.Seed()
 	if err != nil {
-		w.Write([]byte(err.Error()))
+		app.serverError(w, err)
+		return
 	}
 	w.Write([]byte(s))
 }
 
 func (app *application) users(w http.ResponseWriter, r *http.Request) {
-	s, _ := app.remoteConsole.Users()
+	s, err := app.remoteConsole.Users()
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
 	w.Write([]byte(fmt.Sprint(s)))
 }
 
@@ -36,12 +41,13 @@ func (app *application) broadcast(w http.ResponseWriter, r *http.Request) {
 	err := json.NewDecoder(r.Body).Decode(&input)
 	if err != nil {
 		app.errorLog.Fatalf(err.Error())
+		app.clientError(w, http.StatusBadRequest)
 		return
 	}
 
 	output, err := app.remoteConsole.Broadcast(input.Message)
 	if err != nil {
-		w.Write([]byte(err.Error()))
+		app.serverError(w, err)
 	} else {
 		w.Write([]byte(output))
 	}
@@ -56,12 +62,13 @@ func (app *application) message(w http.ResponseWriter, r *http.Request) {
 	err := json.NewDecoder(r.Body).Decode(&input)
 	if err != nil {
 		app.errorLog.Fatalf(err.Error())
+		app.clientError(w, http.StatusBadRequest)
 		return
 	}
 
 	output, err := app.remoteConsole.Message(input.User, input.Message)
 	if err != nil {
-		w.Write([]byte(err.Error()))
+		app.serverError(w, err)
 	} else {
 		w.Write([]byte(output)) // No player was found or You whisper to itsBananas: this is a pm
 	}
