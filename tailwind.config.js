bddcdc1537afc921b30b80272efa51c7a6ec4b5d
Steven Chan <29343843+chansyu@users.noreply.github.com>
2024-08-13 15:26:00 -0700

    setup tailwind & daisyui

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./ui/html/**/*.{html,js}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [
+        require("@tailwindcss/typography"), require('daisyui'),
+    ],
+}
