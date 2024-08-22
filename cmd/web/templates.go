1e409145bbbb2c3c53f462deaae2ec140e8bfd42
Steven Chan <29343843+chansyu@users.noreply.github.com>
2024-08-22 14:22:50 -0700

    transform responses into a table format

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -24,11 +24,19 @@ func humanDate(t time.Time) string {
 	if t.IsZero() {
 		return ""
 	}
-	return t.UTC().Format("02 Jan 2006 at 15:04")
+	return t.UTC().Format("15:04")
+}
+
+func status(s bool) string {
+	if s {
+		return "Success"
+	}
+	return "Error"
 }
 
 var functions = template.FuncMap{
 	"humanDate": humanDate,
+	"status":    status,
 }
 
 func newTemplateCache() (map[string]*template.Template, error) {
