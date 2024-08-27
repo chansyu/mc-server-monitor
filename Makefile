7d81b108a2675c9ec43fecc8c387a86a1406d010
Steven Chan <29343843+chansyu@users.noreply.github.com>
2024-08-26 21:37:06 -0700

    format files with prettier

--- Makefile
+++ Makefile
@@ -11,3 +11,7 @@ build:
 	@npx tailwindcss -i ./ui/static/css/styles.css -o ./ui/static/css/output.css --minify
 	@docker build -t mc-server-monitor-web:multistage -f ./cmd/web/Dockerfile .
 	@docker build -t mc-server-monitor-logs:multistage -f ./cmd/logs/Dockerfile .
+
+format:
+	@gofmt -w .
+	@npx prettier --write .
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./ui/html/**/*.{html,js}"],
-    theme: {
-      extend: {},
-    },
-    plugins: [
-        require("@tailwindcss/typography"), require('daisyui'),
-    ],
-    daisyui: {
-        themes: ["retro"],
-    },
+  content: ['./ui/html/**/*.{html,js}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('@tailwindcss/typography'), require('daisyui')],
+  daisyui: {
+    themes: ['retro'],
+  },
 }
